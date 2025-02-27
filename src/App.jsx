8db0c8232046547434a7a844d6f5ac0b563f7c14
Brendan Dileo <dileob23@gmail.com>
2025-02-26 21:15:16 -0500

    Commit #15 - Adding Routing for New Page Components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import './App.css'
 import "devicon/devicon.min.css";
 
@@ -14,6 +17,9 @@ function App() {
       <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
     </Router>
   )
