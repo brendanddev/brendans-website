f22baebf4c0e25c5aef5099be2becf8afb1b9f03
Brendan Dileo <dileob23@gmail.com>
2025-02-26 19:57:38 -0500

    Commit #10 - Changing Style for Font Color to Match Terminal Theme

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,33 +7,33 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="w-full bg-black border-b border-gray-700 p-4 font-mono text-green-500 flex items-center justify-between">
+    <nav className="w-full bg-black border-b border-gray-700 p-4 font-mono text-[#00ff00] flex items-center justify-between">
       <div>
-        <span className="font-semibold text-xl tracking-tight text-green-500">
+        <span className="font-semibold text-xl tracking-tight text-[#00ff00]">
           Brendan Dileo
         </span>
       </div>
 
       <ul className="flex gap-5 text-lg">
-        <li className="hover:text-green-500">
-          <span className="text-green-500">[</span>
-          <Link to="/" className="text-green-500 hover:text-green-500">home</Link>
-          <span className="text-green-500">]</span>
+        <li className="hover:text-[#00ff00]">
+          <span className="text-[#00ff00]">[</span>
+          <Link to="/" className="text-[#00ff00] hover:text-[#00ff00]">home</Link>
+          <span className="text-[#00ff00]">]</span>
         </li>
-        <li className="hover:text-green-500">
-          <span className="text-green-500">[</span>
-          <Link to="/about" className="text-green-500 hover:text-green-500">about</Link>
-          <span className="text-green-500">]</span>
+        <li className="hover:text-[#00ff00]">
+          <span className="text-[#00ff00]">[</span>
+          <Link to="/about" className="text-[#00ff00] hover:text-[#00ff00]">about</Link>
+          <span className="text-[#00ff00]">]</span>
         </li>
-        <li className="hover:text-green-500">
-          <span className="text-green-500">[</span>
-          <Link to="/projects" className="text-green-500 hover:text-green-500">projects</Link>
-          <span className="text-green-500">]</span>
+        <li className="hover:text-[#00ff00]">
+          <span className="text-[#00ff00]">[</span>
+          <Link to="/projects" className="text-[#00ff00] hover:text-[#00ff00]">projects</Link>
+          <span className="text-[#00ff00]">]</span>
         </li>
-        <li className="hover:text-green-500">
-          <span className="text-green-500">[</span>
-          <Link to="/contact" className="text-green-500 hover:text-green-500">contact</Link>
-          <span className="text-green-500">]</span>
+        <li className="hover:text-[#00ff00]">
+          <span className="text-[#00ff00]">[</span>
+          <Link to="/contact" className="text-[#00ff00] hover:text-[#00ff00]">contact</Link>
+          <span className="text-[#00ff00]">]</span>
         </li>
       </ul>
     </nav>
