728eb99328ef09656e2639be4e4cf033b8a4b9b4
Brendan Dileo <dileob23@gmail.com>
2025-02-27 11:01:00 -0500

    Commit #27 - Implemented State Management for Theme Toggler

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -3,12 +3,22 @@
 // Brendan Dileo
 // To change from dark and light theme on webpage
 
-import { Sun } from "lucide-react";
+import { useState } from "react";
+import { Sun, Moon } from "lucide-react";
 
 const ThemeToggler = () => {
+    const [isDark, setIsDark] = useState(true);     // Dark by default
+
+    const toggleTheme = () => {
+        setIsDark(!isDark);
+    }
+
     return (
-        <button className="fixed bottom-4 left-20 p-2 rounded-full bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white">
-            <Sun size={24} />
+        <button 
+            onClick={toggleTheme}
+            className="fixed bottom-4 left-20 p-2 rounded-full bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white"
+        >
+            {isDark ? <Sun size={30} /> : <Moon size={30} />}
         </button>
     )
 }
