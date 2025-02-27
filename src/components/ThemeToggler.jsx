97627b6ea28c6e26f35aff7acfbf4608141f7573
Brendan Dileo <dileob23@gmail.com>
2025-02-27 10:34:52 -0500

    Commit #26 - Basic Implementation of Sun Icon from Lucide

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -3,10 +3,12 @@
 // Brendan Dileo
 // To change from dark and light theme on webpage
 
+import { Sun } from "lucide-react";
+
 const ThemeToggler = () => {
     return (
         <button className="fixed bottom-4 left-20 p-2 rounded-full bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white">
-            Sun or Moon
+            <Sun size={24} />
         </button>
     )
 }
