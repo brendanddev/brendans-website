c20656ddb8cd3e95d20a05fc50af1e38d0fc52b7
Brendan Dileo <dileob23@gmail.com>
2025-02-26 19:49:09 -0500

    Commit #6 - Adding NavBar for Routing

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+
+
+// NavBar.jsx
+// Brendan Dileo
+
+const NavBar = () => {
+  return (
+    <nav className="w-full bg-black border-b border-gray-700 p-4 font-mono text-green-500 flex items-center justify-between">
+      <div>
+        <span className="font-semibold text-xl tracking-tight text-green-500">
+          Brendan Dileo
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
