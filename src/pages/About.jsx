ed765e20c4d641f3f3623d44bfe782bba2b77d3d
Brendan Dileo <dileob23@gmail.com>
2025-02-27 11:01:53 -0500

    Commit #29 - Beggining to implement About Page with use of Three.JS

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
-
 // About.jsx
 // Brendan Dileo
 
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 const About = () => {
-    return (
-        <h1>About Me</h1>
-    );
+  return (
+    <h1>About Brendan</h1>
+  );
 };
-  
+
 export default About;
