f20500d73c95ae70436bfc88ecde8d84cdf4588c
Brendan Dileo <dileob23@gmail.com>
2025-02-26 19:57:14 -0500

    Commit #9 - Implementing Terminal/CLI Component to Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@
 // Home.jsx
 // Brendan Dileo
 
+// Home functional component
 const Home = () => {
     return (
-      <div className="flex flex-col items-center justify-center h-screen bg-black text-green-500 font-mono">
-        <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-        <p className="mt-4 text-lg">This is a basic homepage to test routing and styling.</p>
-      </div>
+        <div className="w-full min-h-screen bg-gray-900 flex flex-col items-center relative">
+            <h1 className="text-[#00ff00] text-4xl font-mono mb-10">Brendans Website</h1>
+
+            <div className="w-3/5 h-[500px] bg-black rounded-lg shadow-lg flex flex-col border border-gray-700 overflow-hidden font-mono">
+            
+            </div>
+        </div>
     );
   };
   
