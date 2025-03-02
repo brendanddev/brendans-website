7e0d3e4503e24020d17d271229ddc1cf28d37c54
Brendan Dileo <dileob23@gmail.com>
2025-03-02 17:22:14 -0500

    Commit #74 - Documenting Projects File

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,19 +3,29 @@
  * @file Projects.jsx
  * @author Brendan Dileo 
  * 
- * 
+ * The projects page for my portfolio-website.
+ * The Projects page will include the header component, a brief introduction section to the page,
+ * dynamically loaded cards each containing a different project of mine, and the sidebar component.
+ * This page makes use of the React Framer Motion library to create fade in animations when the page
+ * loads.
 */
-
 
 import { motion } from "framer-motion";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import { projectData } from "../projectData";
 
+import { FaFolder } from "react-icons/fa";
 
-
-// FIXME: MessageIcon not working for faq bot card
-
+/**
+ * Projects Functional Component
+ * 
+ * Renders the Projects page, showcasing some of my personal and pasison projects, and info about them.
+ * Dynamically loads project cards based on the data loaded from 'projectData.jsx', and makes use of 
+ * Framer Motion for animation effects.
+ * 
+ * @returns {JSX.Element} The jsx rendering the Projects page.
+ */
 const Projects = () => {
     return (
         <motion.div 
@@ -48,7 +58,9 @@ const Projects = () => {
             </motion.section>
 
             <motion.div className="grid md:grid-cols-3 gap-6 p-6">
+                {/* Loops through the array of projects, creating a card for each project */ }
                 {projectData.map((project, index) => (
+                    // Applies the animation to the generated card
                     <motion.div 
                         key={index}
                         initial={{ scale: 0.8, opacity: 0 }}
@@ -56,15 +68,19 @@ const Projects = () => {
                         transition={{ duration: 0.5, delay: index * 0.2 }}
                         className="w-96 min-h-[250px] bg-black border border-green-500 rounded-lg shadow-sm"
                     >
+                        {/* Loads the data into the card */}
                         <div className="p-6">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-green-400">{project.title}</h5>
                             <p className="mb-4 text-sm font-normal text-gray-400">{project.desc}</p>
                             <div className="flex space-x-4 mb-3 text-green-400">
+                                {/* Iterates through all the icons in the current project card and displays them */}
                                 {project.icons.map((icon, i) => (
                                     <i key={i} className={`${icon} text-3xl`}></i>
                                 ))}
+                                {/* Extra icons to be displayed */}
                                 {project.extraIcon}
                             </div>
+                            {/* Link to the project */}
                             <a 
                                 target="_blank"
                                 rel="noopener noreferrer"
@@ -82,4 +98,5 @@ const Projects = () => {
     );
 };
 
+// Exports the Projects page component
 export default Projects;
