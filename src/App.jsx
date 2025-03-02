7488d2a9d1b1123109cec25db6e28b3dd3aca096
Brendan Dileo <dileob23@gmail.com>
2025-03-02 17:47:49 -0500

    Commit #76 - Documenting App and Header Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 
-// App.jsx
-// Brendan Dileo
+/**
+ * @file App.jsx
+ * @author Brendan Dileo
+ * 
+ * The main entry point of my portfolio-website.
+ * This component is responsible for setting up routing between pages with React Router.
+ * It renders the navigational component (NavBar) and handles routing to different pages
+ * on the site like the Home page, or the About page.
+ * 
+*/
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
@@ -11,10 +19,19 @@ import Contact from './pages/Contact';
 import './App.css'
 import "devicon/devicon.min.css";
 
+/**
+ * Sets up the basic structure of the portfolio-website.
+ * It initializes the React Router, and renders the NavBar and different page routes.
+ * 
+ * @returns {JSX.Element} - The JSX that contains the app layout, navigation and routed pages.
+ */
 function App() {
   return (
+    // Sets up react router
     <Router>
+      {/* Sets up NavBar */}
       <NavBar />
+        {/* Specifies route paths */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -25,4 +42,5 @@ function App() {
   )
 }
 
+// Exports the App component
 export default App;
