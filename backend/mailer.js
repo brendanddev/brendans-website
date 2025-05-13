df13cfac2daacb8a80c15e2a58f0580e81b16c02
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-13 10:02:56 -0400

    Setup NodeMailer transporter for sending emails with GMAIL SMTP

--- backend/mailer.js
+++ backend/mailer.js
@@ -4,3 +4,17 @@
 
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+
+// Node Mailer Transporter setup
+const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: false,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
+});
+
+
+
