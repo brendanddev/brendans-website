fb4e26e00ec080d54f416cb6798c2ce92c29363c
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-12 21:03:33 -0400

    (Issue) - Database Object not created with Common Constructor

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,24 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 
+const db = new betterSqlite3.Database('./submissons.db', (err) => {
+    if (err) {
+        console.error('An error occurred while opening the database: ', err.message);
+    }
+    console.log('Connected to the database.');
+});
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS submissons (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      email TEXT NOT NULL,
+      phone TEXT,
+      comment TEXT,
+      date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+`);
+
 app.get('/', (req, res) => {
     console.log('GET Route Successful');
 });
