29ad84fe973fc704770c5f33b26237c7cbdea158
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-17 17:18:35 -0400

    Install declaraction packages and setup config

--- frontend/src/context/ThemeContext.tsx
+++ frontend/src/context/ThemeContext.tsx
@@ -4,3 +4,11 @@
  * @author Brendan Dileo
  */
 
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+
+
+interface ThemeContextType {
+   isDarkMode: boolean;
+   toggleTheme: () => void;
+}
+
