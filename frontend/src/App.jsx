23a752630833119f00784a1228e93d2d8abdf654
Brendan Dileo <dileob23@gmail.com>
2025-06-24 11:37:02 -0400

    Remove me page from nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import Me from './pages/Me';
 import './App.css'
 import "devicon/devicon.min.css";
 
@@ -29,7 +28,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/me" element={<Me />} />
         </Routes>
     </Router>
   )
