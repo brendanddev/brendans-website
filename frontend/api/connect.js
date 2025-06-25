931e87b535fe0fda09bf51d58489798bfabf7c11
Brendan Dileo <dileob23@gmail.com>
2025-06-25 19:03:09 -0400

    Implement basic router handler for submitting messages

--- frontend/api/connect.js
+++ frontend/api/connect.js
@@ -1,1 +1,25 @@
 
+// connect.js
+// Brendan Dileo, June 2025
+
+import supabase from './client';
+
+export default async function handler(req, res) {
+
+    if (req.method !== 'POST')
+        return res.status(405).json({ message: 'Incorrect request type!' });
+
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message)
+        return res.status(400).json({ message: 'Missing one of the required fields!' });
+
+    const { data, error } = await supabase
+        .from('messages')
+        .insert([{ name, email, message }]);
+    
+    if (error)
+        return res.status(500).json({ message: 'An error occurred!' });
+
+    res.status(201).json({ message: 'Message saved!', data });
+}
