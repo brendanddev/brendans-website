ad302af760ad66103f2703d35576ff9f17efbbde
Brendan Dileo <dileob23@gmail.com>
2025-06-25 18:58:17 -0400

    Implement supabase client

--- frontend/api/client.js
+++ frontend/api/client.js
@@ -3,3 +3,12 @@
 // Brendan Dileo, June 2025
 
 import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPBASE_KEY
+);
+
+export default supabase;
+
+
