749551fe9b8fb72a2a5263c1af0268316a9346f9
Brendan Dileo <dileob23@gmail.com>
2025-06-30 14:33:35 -0400

    Implement mobile components

---
+++ frontend/src/components/Navbar/MobileMenu.jsx
@@ -0,0 +1,34 @@
+
+/**
+ * @file MobileMenu.jsx
+ * @author Brendan Dileo, June 2025
+ * 
+ * Renders the dropdown menu for mobile devices in the navbar.
+ */
+
+
+import { AnimatePresence, motion } from "framer-motion";
+import NavLinks from "./NavLinks";
+import { mobileMenuVariants } from "./variants";
+
+const MobileMenu = ({ isOpen, closeMenu }) => (
+    // Mobile menu that appears when the hamburger icon is clicked
+    <AnimatePresence>
+        {/* Displays menu items if the menu is open */}
+        {isOpen && (
+            <motion.div
+                variants={mobileMenuVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                className="md:hidden border-t border-gray-700 bg-black mt-4"
+            >
+                <ul className="py-4 space-y-2 font-mono text-lg">
+                <NavLinks isMobile={true} closeMenu={closeMenu} />
+                </ul>
+            </motion.div>
+        )}
+    </AnimatePresence>
+);
+
+export default MobileMenu;
