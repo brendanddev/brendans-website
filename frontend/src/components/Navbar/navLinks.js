9c926c5b760bf070ff1e95a0232901c84bfde002
Brendan Dileo <dileob23@gmail.com>
2025-06-30 13:15:41 -0400

    Create and implement nav links file

---
+++ frontend/src/components/Navbar/navLinks.js
@@ -0,0 +1,15 @@
+
+/**
+ * @file navLinks.js
+ * @author Brendan Dileo, June 2025
+ * 
+ * Defines and exports an array links used in the nav component.
+ * Each link points to a different page within the website.
+ */
+
+export const navLinks = [
+    { to: "/", label: "home" },
+    { to: "/about", label: "about" },
+    { to: "/projects", label: "projects" },
+    { to: "/contact", label: "contact" },
+];
