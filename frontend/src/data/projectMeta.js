ecfc06c5a753a466773974d5981c783430c86a5d
Brendan Dileo <dileob23@gmail.com>
2025-07-03 18:14:37 -0400

    Update project meta to include design types

---
+++ frontend/src/data/projectMeta.js
@@ -0,0 +1,28 @@
+
+/**
+ * @file projectMeta.js
+ * @author Brendan Dileo, July 2025
+ * 
+ * Basic metadata for my projects
+ */
+
+
+// Projects status
+export const projectStatus = {
+
+  Complete: {
+    name: 'Complete',
+    color: 'from-blue-400 to-blue-600'
+  },
+
+  'In Progress': {
+    name: 'In Progress',
+    color: 'from-yellow-400 to-yellow-600'
+  },
+  
+  Prototype: {
+    name: 'Prototype',
+    color: 'from-gray-400 to-gray-600'
+  }
+
+}; 
