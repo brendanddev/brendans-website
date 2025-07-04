cdb1f2a48212674d8bf3bb3452f05f40043cb61c
Brendan Dileo <dileob23@gmail.com>
2025-07-04 18:59:37 -0400

    Update doc comment for project modal index

--- frontend/src/components/Projects/ProjectModal/index.js
+++ frontend/src/components/Projects/ProjectModal/index.js
@@ -2,6 +2,10 @@
 /**
  * @file index.js
  * @author Brendan Dileo, July 2025
+ * 
+ * Acts as the barrel filer for the ProjectModal component,
+ * re exporting the default export from the ProjectModal file 
+ * to allow cleaner and simpler import statements in other files
  */
 
 export { default } from './ProjectModal';
