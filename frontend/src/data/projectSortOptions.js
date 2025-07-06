4e2d010c828c89c6b32c7055fed83bc97d0045df
Brendan Dileo <dileob23@gmail.com>
2025-07-06 11:30:34 -0400

    Setup project sort options

---
+++ frontend/src/data/projectSortOptions.js
@@ -0,0 +1,23 @@
+
+/**
+ * @file projectSortOptions.js
+ * @author Brendan Dileo, July 2025
+ * 
+ * 
+ */
+
+
+
+import { FaSortAlphaUp, FaSortAlphaDown, FaClock, FaCalendarAlt } from "react-icons/fa";
+
+/**
+ * newest - new icon
+ * Oldes - calendar icon or arrow down?
+ * Name from a - z -- abc icon or some sort of arrow
+ * Name from z - a -- abc icon or some sort of arrow pointing up
+ */
+
+const sortOptions = [
+];
+
+export default sortOptions;
