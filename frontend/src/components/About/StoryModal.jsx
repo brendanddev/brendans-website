4304e6ab3ad794b36fc11765b48520c3b37407de
Brendan Dileo <dileob23@gmail.com>
2025-07-10 21:14:08 -0400

    Setup story modal component

---
+++ frontend/src/components/About/StoryModal.jsx
@@ -0,0 +1,17 @@
+
+/**
+ * @file StoryModal.jsx
+ * @author Brendan Dileo
+ * 
+ * Enhanced modal component for telling my personal story with rich content
+*/
+
+import { motion } from "framer-motion";
+import Modal from "../common/Modal";
+
+const StoryModal = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+      
+};
+
+export default StoryModal; 
