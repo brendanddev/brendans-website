0195a9623714726737dfa222710252883563e22f
Brendan Dileo <dileob23@gmail.com>
2025-07-10 20:19:53 -0400

    Create education card component

---
+++ frontend/src/components/About/EducationCard.jsx
@@ -0,0 +1,8 @@
+
+/**
+ * @file EducationCard.jsx
+ * @author Brendan Dileo
+ * 
+ * Compnent for displaying details about education, achievements, and honors
+ * on the about page of my portfolio website
+*/
