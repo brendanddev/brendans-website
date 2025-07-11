761f577ac48f2df3a9bfcfbda4dbc55cdc7657b2
Brendan Dileo <dileob23@gmail.com>
2025-07-11 09:01:45 -0400

    Update skills data to include skill object arrays

---
+++ frontend/src/data/skillsData.js
@@ -0,0 +1,50 @@
+
+/**
+ * @file skillsData.js
+ * @author Brendan Dileo, July 2025
+ * 
+ * Defines the arrays of skill icon data to be categorized and used inside the 
+ * SkillsCard component on the about page
+ */
+
+
+// Defines the programming skills objects
+const programmingLanguages = [
+    { className: "devicon-java-plain", hoverColor: "#ff9900" },
+    { className: "devicon-python-plain", hoverColor: "#306998" },
+    { className: "devicon-javascript-plain", hoverColor: "#f7df1e" },
+    { className: "devicon-csharp-plain", hoverColor: "#9b4f96" },
+    { className: "devicon-dot-net-plain", hoverColor: "#512bd4" },
+    { className: "devicon-php-plain", hoverColor: "#777bb3" },
+    { className: "devicon-swift-plain", hoverColor: "#fa7343" }
+];
+
+// Defines web dev skill objects
+const webDevelopment = [
+    { className: "devicon-react-original", hoverColor: "#61dafb" },
+    { className: "devicon-nodejs-plain-wordmark", hoverColor: "#68a063" },
+    { className: "devicon-bootstrap-plain", hoverColor: "#563d7c" },
+    { className: "devicon-tailwindcss-original", hoverColor: "#38b2ac" },
+    { className: "devicon-html5-plain", hoverColor: "#e34c26" },
+    { className: "devicon-css3-plain", hoverColor: "#1572b6" }
+];
+
+// Defines db and game dev skill objects
+const databasesAndGameDev = [
+    { className: "devicon-microsoftsqlserver-plain", hoverColor: "#cc2927" },
+    { className: "devicon-mysql-plain", hoverColor: "#00758f" },
+    { className: "devicon-bash-plain", hoverColor: "#4eaa25" },
+    { className: "devicon-unity-plain", hoverColor: "#ffffff" },
+    { className: "devicon-godot-plain", hoverColor: "#478cbf" }
+];
+
+const toolsAndSoftware = [
+    { className: "devicon-git-plain", hoverColor: "#f34f29" },
+    { className: "devicon-vscode-plain", hoverColor: "#007acc" },
+    { className: "devicon-pycharm-plain", hoverColor: "#21d789" },
+    { className: "devicon-json-plain", hoverColor: "#f7df1e" },
+    { className: "devicon-intellij-plain", hoverColor: "#000000" },
+    { className: "devicon-gimp-plain", hoverColor: "#5c5543" },
+    { className: "devicon-xcode-plain", hoverColor: "#1575f3" },
+    { className: "devicon-linux-plain", hoverColor: "#fcc624" }
+];
