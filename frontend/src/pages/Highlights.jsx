704e639a17d19edcaa3f199cf6799484dd90306c
Brendan Dileo <dileob23@gmail.com>
2025-07-16 17:44:11 -0400

    Setup basic highlights page

--- frontend/src/pages/Highlights.jsx
+++ frontend/src/pages/Highlights.jsx
@@ -6,9 +6,22 @@
  * The highlights page for my website
  */
 
+import Header from "../components/common/Header";
+import PageWrapper from "../components/common/PageWrapper";
 import DottedGridSection from "../components/Highlights/DottedGridSection";
 
 const Highlights = () => {
+
+    return (
+        <PageWrapper>
+            <div className="relative z-10 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <Header 
+                    title="Highlights" 
+                    subtitle="A showcase of my key achievements and experiences."
+                />
+            </div>
+        </PageWrapper>
+    );
 };
 
 export default Highlights;
