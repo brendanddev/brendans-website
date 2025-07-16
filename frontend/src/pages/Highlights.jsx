03218c6a9f3a5062eb2bb9c63da74799038b3af5
Brendan Dileo <dileob23@gmail.com>
2025-07-16 17:49:36 -0400

    Add dotted grid component to highlights page

--- frontend/src/pages/Highlights.jsx
+++ frontend/src/pages/Highlights.jsx
@@ -9,6 +9,7 @@
 import Header from "../components/common/Header";
 import PageWrapper from "../components/common/PageWrapper";
 import DottedGridSection from "../components/Highlights/DottedGridSection";
+import SkillProficiencySection from "../components/Highlights/SkillProficiencySection";
 
 const Highlights = () => {
 
@@ -16,9 +17,14 @@ const Highlights = () => {
         <PageWrapper>
             <div className="relative z-10 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <Header 
-                    title="Highlights" 
+                    title="Highlights"
                     subtitle="A showcase of my key achievements and experiences."
                 />
+            </div>
+            <div className="mt-8 sm:mt-12 px-4 sm:px-6 lg:px-8">
+                
+                {/* Dotted grid section showcasing key highlights */}
+                <DottedGridSection />
             </div>
         </PageWrapper>
     );
