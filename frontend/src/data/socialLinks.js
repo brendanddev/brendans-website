0c79ddeb1d35c0dc5b1d49b2a1f2bd433d2fd12a
Brendan Dileo <dileob23@gmail.com>
2025-09-10 18:01:44 -0400

    Create social links file and update with mock social data

---
+++ frontend/src/data/socialLinks.js
@@ -0,0 +1,31 @@
+
+/**
+ * @file socialLinks.js
+ * @author Brendan Dileo
+ * 
+ * Defines and exports social media links for use in the sidebar component.
+ */
+
+// FILLER DATA
+export const socialLinks = [
+    {
+        name: 'GitHub',
+        url: 'https://github.com/brendan-dileo',
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://linkedin.com/in/brendan-dileo',
+    },
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/brendan_dileo',
+    },
+    {
+        name: 'Email',
+        url: 'mailto:brendan.dileo@mohawkcollege.ca',
+    },
+    {
+        name: 'Resume',
+        url: '/BrendanDileo_Resume.pdf',
+    }
+];
