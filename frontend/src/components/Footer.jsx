9c1201c2dcb3b0ad35bfb58d566a9531a6db792a
Brendan Dileo <dileob23@gmail.com>
2025-09-12 08:22:02 -0400

    Create Footer component to display social links as a replacement to the Sidebar

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+/**
+ * @file Footer.jsx
+ * @author Brendan Dileo, September 2025
+ * 
+ * A footer component displaying social media links with interactive icons.
+ */
+
+
+import { memo } from 'react';
+import { motion } from 'framer-motion';
