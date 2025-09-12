d88581c663a92db015089986c336f1ca7461c8d8
Brendan Dileo <dileob23@gmail.com>
2025-09-12 14:51:40 -0400

    Create terminalCommands to hold commands and responses

---
+++ frontend/src/data/terminalCommands.js
@@ -0,0 +1,9 @@
+
+/**
+ * @file terminalCommands.js
+ * @author Brendan Dileo, September 2025
+ * 
+ * Defines and exports terminal command responses for the Terminal component on the Home page.
+ */
+
+
