01ce5f6011aa3e538aa9787646547834e4f4f559
Brendan Dileo <dileob23@gmail.com>
2025-09-23 12:11:45 -0400

    Add homepage test to verify title and subtitle are loaded and visible

--- frontend/tests/homepage.spec.js
+++ frontend/tests/homepage.spec.js
@@ -6,8 +6,17 @@
  * Defines tests for the homepage of the website.
  */
 
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('Homepage loads correctly', async ({ page }) => {
+test('Homepage h1 loads correctly', async ({ page }) => {
     await page.goto('https://brendans-website.vercel.app/');
+
+    const h1 = page.locator('h1');
+    await expect(h1).toHaveText('BRENDAN DILEO');
+});
+
+test('Homepage subtitle is visible', async ({ page }) => {
+    await page.goto('https://brendans-website.vercel.app/');
+    const subtitle = page.locator('p', { hasText: 'Software Developer | Tech Enthusiast' });
+    await expect(subtitle).toBeVisible();
 });
