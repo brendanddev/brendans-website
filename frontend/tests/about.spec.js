0db2ac1ed9df8be9c3b7c4d1961702ff5fcf0e9a
Brendan Dileo <dileob23@gmail.com>
2025-09-23 15:49:19 -0400

    Create about tests file

---
+++ frontend/tests/about.spec.js
@@ -0,0 +1,9 @@
+
+/**
+ * @file about.spec.js
+ * @author Brendan Dileo, September 2025
+ * 
+ * Defines tests for the about page of the website.
+ */
+
+import { expect, test } from '@playwright/test';
