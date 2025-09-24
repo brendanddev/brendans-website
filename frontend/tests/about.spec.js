e054a8c6e5aa565c1ac3bd35cb8b0bef99286382
Brendan Dileo <dileob23@gmail.com>
2025-09-24 19:35:00 -0400

    Add tests to validate about page content

--- frontend/tests/about.spec.js
+++ frontend/tests/about.spec.js
@@ -10,10 +10,21 @@ import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('https://brendans-website.vercel.app');
+    await page.click('a[href="/about"]');
 });
 
 test('About page h1 loads correctly', async ({ page }) => {
-    await page.click('a[href="/about"]');
     const h1 = page.locator('h1');
     await expect(h1).toHaveText('ABOUT ME');
+});
+
+test('About page subtitle loads correctly', async ({ page }) => {
+    await page.click('a[href="/about"]');
+    const subtitle = page.locator('p.text-lg');
+    await expect(subtitle).toHaveText('Get to know me');
+});
+
+test('About page education section is visible', async ({ page }) => {
+    const header = page.getByRole('heading', { name: 'education' });
+    await expect(header).toBeVisible();
 });
