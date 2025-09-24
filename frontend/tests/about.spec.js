6a1efb5c4d2a68471660a2d955c48326cc3e070a
Brendan Dileo <dileob23@gmail.com>
2025-09-23 20:36:01 -0400

    Add basic about page test

--- frontend/tests/about.spec.js
+++ frontend/tests/about.spec.js
@@ -7,3 +7,12 @@
  */
 
 import { expect, test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://brendans-website.vercel.app/about');
+});
+
+test('About page h1 loads correctly', async ({ page }) => {
+    const h1 = page.locator('h1');
+    await expect(h1).toHaveText('ABOUT ME');
+});
