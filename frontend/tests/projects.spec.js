b4fda7029d5bc5751409369c8d013085e10ae42e
Brendan Dileo <dileob23@gmail.com>
2025-09-26 08:45:11 -0400

    Setup basic tests for the projects page

--- frontend/tests/projects.spec.js
+++ frontend/tests/projects.spec.js
@@ -6,3 +6,21 @@
  * Defines tests for the projects page of the website.
  */
 
+// npx playwright test
+
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://brendans-website.vercel.app');
+  await page.click('a[href="/projects"]');
+});
+
+test('Projects page h1 loads correctly', async ({ page }) => {
+  const h1 = page.locator('h1');
+  await expect(h1).toHaveText('MY PROJECTS');
+});
+
+test('Projects page subtitle loads correctly', async ({ page }) => {
+  const subtitle = page.locator('p.text-lg');
+  await expect(subtitle).toHaveText('Explore my work');
+});
