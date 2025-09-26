b2ba73251d2fa93e4dd283aa424e3dd5f9c2b28a
Brendan Dileo <dileob23@gmail.com>
2025-09-26 09:42:14 -0400

    Setup basic tests for contact page

--- frontend/tests/contact.spec.js
+++ frontend/tests/contact.spec.js
@@ -6,3 +6,22 @@
  * Defines tests for the contact page of the website.
  */
 
+
+// npx playwright test
+
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://brendans-website.vercel.app');
+  await page.click('a[href="/contact"]');
+});
+
+test('Contact page h1 loads correctly', async ({ page }) => {
+  const h1 = page.locator('h1');
+  await expect(h1).toHaveText('CONTACT ME');
+});
+
+test('Contact page subtitle loads correctly', async ({ page }) => {
+  const subtitle = page.locator('p.text-lg');
+  await expect(subtitle).toHaveText("Let's work together");
+});
